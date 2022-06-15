678d31c3e3633d6a749a937ba02c7cc1fb9f7d5d
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-06-15 14:33:57 +0300

    Up to exercise 3.15

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const Phonebook = require("./models/mongo");
 
 // JSON parser
 app.use(express.json());
@@ -39,28 +40,6 @@ app.use(
     }
   )
 );
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 // Main page
 app.get("/", (request, response) => {
@@ -69,22 +48,27 @@ app.get("/", (request, response) => {
 
 // Get the list of persons in JSON format
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Phonebook.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 // Get the person with the given id in JSON format
 app.get("/api/persons/:id", (request, response) => {
   // Set the id variable to the request parameter
-  const id = Number(request.params.id);
+  const id = String(request.params.id);
   // Find the person with the given id
-  const person = persons.find((person) => person.id === id);
-  // If the person is found, return the person in JSON format
-  if (person) {
-    response.json(person);
-    // If the person is not found, return 404
-  } else {
-    response.status(404).end();
-  }
+  Phonebook.findById(id)
+    .exec()
+    .then((person) => {
+      // If the person is found, return the person in JSON format
+      if (person) {
+        response.json(person);
+        // If the person is not found, return 404
+      } else {
+        response.status(404).end();
+      }
+    });
 });
 
 // Get the length of the list of persons and the current date
@@ -101,12 +85,12 @@ app.get("/info", (request, response) => {
 // Delete a person with the given id from the list
 app.delete("/api/persons/:id", (request, response) => {
   // Set the id variable to the request parameter
-  const id = Number(request.params.id);
+  const id = String(request.params.id);
   // Find the person with the given id
-  const person = persons.find((person) => person.id === id);
+  const person = Phonebook.findById(id).exec();
   // If the person is found, remove the person from the list
   if (person) {
-    persons = persons.filter((person) => person.id !== id);
+    Phonebook.findByIdAndRemove(id).exec();
     response.status(204).end();
     // If the person is not found, return 404
   } else {
@@ -124,36 +108,38 @@ app.post("/api/persons", (request, response) => {
       error: "name or number missing",
     });
   }
-  // If the name is already in the list, return 400
-  const duplicate = persons.find((person) => person.name === body.name);
-  if (duplicate) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-  // If the number is already in the list, return 400
-  const duplicateNumber = persons.find(
-    (person) => person.number === body.number
-  );
-  if (duplicateNumber) {
-    return response.status(400).json({
-      error: "number must be unique",
-    });
-  }
+
+  // DUPLICATE CHECK
+
+  // // If the name is already in the list, return 400
+  // const duplicateName = Phonebook.find({ name: body.name });
+  // if (duplicateName) {
+  //   return response.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
+  // // If the number is already in the list, return 400
+  // const duplicateNumber = Phonebook.find({ number: body.number });
+  // if (duplicateNumber) {
+  //   return response.status(400).json({
+  //     error: "number must be unique",
+  //   });
+  // }
+
   // If the name and number are given, add the person to the list
-  const person = {
-    // Generate a random id
-    id: Math.floor(Math.random() * 1000),
+  const person = new Phonebook({
     name: body.name,
     number: body.number,
-  };
-  persons = persons.concat(person);
-  response.json(person);
+  });
+
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 let port = process.env.PORT;
 if (port == null || port === "") {
-  port = 8000;
+  port = 3001;
 }
 
 app.listen(port, () => {
---
+++ models/mongo.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// Get the password from the environment variable
+// const password = process.env.MONGO_PASSWORD;
+// URL to the database
+const url = process.env.MONGO_URI;
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error.message);
+  });
+
+// SCHEMA
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+// Remove the id and version fields from the schema
+phonebookSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("PhonebookEntries", phonebookSchema);

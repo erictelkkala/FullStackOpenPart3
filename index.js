a301fa983f0983bbce3d11287f6c50bd7d72fc5d
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-07-03 13:10:51 +0300

    Part 3 completed completely
    JS files have been linted

--- index.js
+++ index.js
@@ -1,223 +1,223 @@
-const express = require("express");
-const app = express();
-const Phonebook = require("./models/mongo");
+const express = require('express')
+const app = express()
+const Phonebook = require('./models/mongo')
 
 // CORS
-const cors = require("cors");
+const cors = require('cors')
 
 // Morgan
-const morgan = require("morgan");
+const morgan = require('morgan')
 
 // Morgan token for POST requests
-morgan.token("body", (req, res) => {
-  return JSON.stringify(req.body);
-});
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
 
 // Static files
-app.use(express.static("build"));
+app.use(express.static('build'))
 // JSON parser
-app.use(express.json());
+app.use(express.json())
 // CORS
-app.use(cors());
+app.use(cors())
 
 const logger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 
-app.use(logger);
+app.use(logger)
 
 // Morgan
 app.use(
-  morgan("tiny", {
-    // Skip the tine format if the request is POST
-    skip: function (req, res) {
-      return req.method === "POST";
-    },
-  })
-);
+    morgan('tiny', {
+        // Skip the tine format if the request is POST
+        skip: function (req) {
+            return req.method === 'POST'
+        },
+    })
+)
 // Log the body of POST requests
 app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :body",
-    {
-      // Skip if the request is not POST
-      skip: function (req, res) {
-        return req.method !== "POST";
-      },
-    }
-  )
-);
+    morgan(
+        ':method :url :status :res[content-length] - :response-time ms :body',
+        {
+            // Skip if the request is not POST
+            skip: function (req) {
+                return req.method !== 'POST'
+            },
+        }
+    )
+)
 
 // Main page
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
 
 // Get the list of persons in JSON format
-app.get("/api/persons", (request, response) => {
-  Phonebook.find({})
-    .then((persons) => {
-      response.json(persons);
-    })
-    .catch((error) => {
-      response.status(400).send(error);
-    });
-});
+app.get('/api/persons', (request, response) => {
+    Phonebook.find({})
+        .then((persons) => {
+            response.json(persons)
+        })
+        .catch((error) => {
+            response.status(400).send(error)
+        })
+})
 
 // Get the person with the given id in JSON format
-app.get("/api/persons/:id", (request, response, next) => {
-  // Set the id variable to the request parameter
-  const id = String(request.params.id);
-  // Find the person with the given id
-  Phonebook.findById(id)
-    .exec()
-    .then((person) => {
-      // If the person is found, return the person in JSON format
-      if (person) {
-        response.json(person);
-      }
-    })
-    // If an error occurs, pass it to the error handler
-    .catch((error) => next(error));
-});
+app.get('/api/persons/:id', (request, response, next) => {
+    // Set the id variable to the request parameter
+    const id = String(request.params.id)
+    // Find the person with the given id
+    Phonebook.findById(id)
+        .exec()
+        .then((person) => {
+            // If the person is found, return the person in JSON format
+            if (person) {
+                response.json(person)
+            }
+        })
+        // If an error occurs, pass it to the error handler
+        .catch((error) => next(error))
+})
 
 // Get the length of the list of persons and the current date
-app.get("/info", (request, response, next) => {
-  // Get the current date
-  const date = new Date();
+app.get('/info', (request, response, next) => {
+    // Get the current date
+    const date = new Date()
 
-  // Find the number of persons in the database
-  Phonebook.countDocuments({}, function (err, count) {
-    if (err) {
-      next(err);
-    }
-    try {
-      // Send the number of persons and the current date in JSON format as the response
-      response.send(`<p>Phonebook has info for ${count} people</p>
-    <p>${date}</p>`);
-    } catch (error) {
-      next(error);
-    }
-  });
-});
+    // Find the number of persons in the database
+    Phonebook.countDocuments({}, function (err, count) {
+        if (err) {
+            next(err)
+        }
+        try {
+            // Send the number of persons and the current date in JSON format as the response
+            response.send(`<p>Phonebook has info for ${count} people</p>
+    <p>${date}</p>`)
+        } catch (error) {
+            next(error)
+        }
+    })
+})
 
 // Delete a person with the given id from the list
-app.delete("/api/persons/:id", (request, response, next) => {
-  // Set the id variable to the request parameter
-  const id = String(request.params.id);
-  // Find the person with the given id
-  Phonebook.findById(id)
-    .exec()
-    .then((person) => {
-      // If the person is found, remove the person from the list
-      if (person) {
-        Phonebook.findByIdAndRemove(id).exec();
-        response.status(204).end();
-        // If the person is not found, return 404
-      } else {
-        response.status(404).end();
-      }
-    })
-    // If an error occurs, pass it to the error handler
-    .catch((error) => next(error));
-});
+app.delete('/api/persons/:id', (request, response, next) => {
+    // Set the id variable to the request parameter
+    const id = String(request.params.id)
+    // Find the person with the given id
+    Phonebook.findById(id)
+        .exec()
+        .then((person) => {
+            // If the person is found, remove the person from the list
+            if (person) {
+                Phonebook.findByIdAndRemove(id).exec()
+                response.status(204).end()
+                // If the person is not found, return 404
+            } else {
+                response.status(404).end()
+            }
+        })
+        // If an error occurs, pass it to the error handler
+        .catch((error) => next(error))
+})
 
 // Add a new person to the list
-app.post("/api/persons", (request, response) => {
-  // Get the body of the request
-  const body = request.body;
-  // If the name and number are not given, return 400
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or number missing",
-    });
-  }
+app.post('/api/persons', (request, response) => {
+    // Get the body of the request
+    const body = request.body
+    // If the name and number are not given, return 400
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing',
+        })
+    }
 
-  // If the name and number are given, add the person to the list
-  const person = new Phonebook({
-    name: body.name,
-    number: body.number,
-  });
-
-  person
-    .save()
-    .then((savedPerson) => {
-      response.json(savedPerson);
+    // If the name and number are given, add the person to the list
+    const person = new Phonebook({
+        name: body.name,
+        number: body.number,
     })
-    .catch((error) => {
-      response.status(400).send(error);
-    });
-});
+
+    person
+        .save()
+        .then((savedPerson) => {
+            response.json(savedPerson)
+        })
+        .catch((error) => {
+            response.status(400).send(error)
+        })
+})
 
 // Modify a person with the given id
-app.put("/api/persons/:id", (request, response, next) => {
-  // Set the id variable to the request parameter
-  const id = String(request.params.id);
-  // Get the body of the request
-  const body = request.body;
-  // If the name and number are not given, return 400
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or number missing",
-    });
-  }
+app.put('/api/persons/:id', (request, response, next) => {
+    // Set the id variable to the request parameter
+    const id = String(request.params.id)
+    // Get the body of the request
+    const body = request.body
+    // If the name and number are not given, return 400
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing',
+        })
+    }
 
-  // Options for validation update function
-  const opts = { runValidators: true, context: "query" };
+    // Options for validation update function
+    const opts = { runValidators: true, context: 'query' }
 
-  // If the name and number are given, modify the person with the given id
-  Phonebook.findByIdAndUpdate(
-    id,
-    {
-      name: body.name,
-      number: body.number,
-    },
-    opts
-  )
-    .then(
-      (person) => {
-        // If the person is found, return the person in JSON format
-        if (person) {
-          response.json(person.toJSON());
-        }
-      }
-      // If an error occurs, pass it to the error handler
+    // If the name and number are given, modify the person with the given id
+    Phonebook.findByIdAndUpdate(
+        id,
+        {
+            name: body.name,
+            number: body.number,
+        },
+        opts
     )
-    .catch((error) => next(error));
-});
+        .then(
+            (person) => {
+                // If the person is found, return the person in JSON format
+                if (person) {
+                    response.json(person.toJSON())
+                }
+            }
+            // If an error occurs, pass it to the error handler
+        )
+        .catch((error) => next(error))
+})
 
 // Unknown route
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 // Error handling
 const errorHandler = (error, request, response, next) => {
-  console.error("Mongoose error: " + error.message);
+    console.error('Mongoose error: ' + error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send("malformed ID");
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
+    if (error.name === 'CastError') {
+        return response.status(400).send('malformed ID')
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
-  next(error);
-};
+    next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 // Configure the port to look for the environment variable PORT, otherwise use port 3001
-let port = process.env.PORT;
-if (port == null || port === "") {
-  port = 3001;
+let port = process.env.PORT
+if (port == null || port === '') {
+    port = 3001
 }
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+    console.log(`Server running on port ${port}`)
+})
--- models/mongo.js
+++ models/mongo.js
@@ -1,50 +1,50 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 // Get the password from the environment variable
 // const password = process.env.MONGO_PASSWORD;
 // URL to the database
-const url = process.env.MONGO_URI;
+const url = process.env.MONGO_URI
 
 mongoose
-  .connect(url)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("Error connecting to MongoDB: ", error.message);
-  });
+    .connect(url)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('Error connecting to MongoDB: ', error.message)
+    })
 
 // SCHEMA
 const phonebookSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 3,
-    required: [true, "Name is required"],
-  },
-  number: {
-    type: String,
-    required: [true, "Phone number is required"],
-  },
-});
+    name: {
+        type: String,
+        minlength: 3,
+        required: [true, 'Name is required'],
+    },
+    number: {
+        type: String,
+        required: [true, 'Phone number is required'],
+    },
+})
 
 // Number validator
 const numberValidator = (v) => {
-  return /[0-9]{1,3}-[0-9]{6,10}/.test(v);
-};
+    return /[0-9]{1,3}-[0-9]{6,10}/.test(v)
+}
 
 // Apply the validator to the phone number field
 phonebookSchema
-  .path("number")
-  .validate(numberValidator, "`{VALUE}` is an invalid phone number");
+    .path('number')
+    .validate(numberValidator, '`{VALUE}` is an invalid phone number')
 
 // Remove the id and version fields from the schema
-phonebookSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+phonebookSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    },
+})
 
-module.exports = mongoose.model("PhonebookEntries", phonebookSchema);
+module.exports = mongoose.model('PhonebookEntries', phonebookSchema)
